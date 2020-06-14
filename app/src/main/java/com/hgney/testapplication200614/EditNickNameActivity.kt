af0eb9faa167691b632a995381931d3e2de03be2
khkssong6949 <bada6949@gmial.com>
2020-06-14 15:24:17 +0900

    닉네임 변경 버튼 기능 추가

---
+++ app/src/main/java/com/hgney/testapplication200614/EditNickNameActivity.kt
@@ -0,0 +1,25 @@
+package com.hgney.testapplication200614
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nick_name.*
+
+class EditNickNameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nick_name)
+
+        editOKBtn.setOnClickListener {
+
+            val newNickName = toBeEditTxt.text.toString()
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("nick", newNickName)
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+
+        }
+    }
+}
--- app/src/main/java/com/hgney/testapplication200614/MainActivity.kt
+++ app/src/main/java/com/hgney/testapplication200614/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.hgney.testapplication200614
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val REQ_FOR_NICK = 1001
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        changeNickBtn.setOnClickListener {
+            val myIntent = Intent(this, EditNickNameActivity::class.java)
+            startActivityForResult(myIntent, REQ_FOR_NICK)
+        }
     }
 }
